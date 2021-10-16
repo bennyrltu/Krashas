ab9a4af7e35373e86c65b3bb9caf50d7ac5dddd7
Benas <benasrudnc@gmail.com>
2021-10-16 18:56:47 +0300

    Initial commit

---
+++ app/src/main/java/edu/ktu/krashas/User.java
@@ -0,0 +1,17 @@
+package edu.ktu.krashas;
+
+public class User {
+    public String fullName, age, email;
+
+    public User()
+    {
+
+    }
+
+    public User(String fullName, String age, String email)
+    {
+        this.fullName = fullName;
+        this.age = age;
+        this.email = email;
+    }
+}
